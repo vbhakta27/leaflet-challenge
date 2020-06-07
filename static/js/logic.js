d6889ed7e86f765dba65314fe371470139ee22c7
Beast Mode <vikashbhakta27@gmail.com>
2020-06-06 20:21:16 -0500

    formatted popup

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,40 +2,31 @@
 var url1 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var url2 = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-// Perform a GET request to the URL
+// Perform a GET request to the URL for earthquake data
 d3.json(url1, function (data1) {
-  // Once we get a response, send the data.features object to the createFeatures function
+  // Send the data.features object to the createFeaturesEarthquakes function
   var earthquakes = createFeaturesEarthquakes(data1.features)
 
+  // Perform a GET request to the URL for tectonics data
   d3.json(url2, function (data2) {
+    // Send the data.features object to the createFeaturesTectonics function
     var tectonics = createFeaturesTectonics(data2.features)
 
     // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes,tectonics)
+    createMap(earthquakes, tectonics)
   })
 });
-
-function createFeaturesTectonics(tectonicData) {
-  console.log(tectonicData)
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  var tectonics = L.geoJSON(tectonicData, {
-  });
-  return tectonics
-};
-
 
 
 function createFeaturesEarthquakes(earthquakeData) {
   console.log(earthquakeData)
-
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   var earthquakes = L.geoJSON(earthquakeData, {
     pointToLayer: function (geoJsonPoint, latlng) {
       return L.circleMarker(latlng, {
         fillOpacity: 0.75,
-        color: "white",
-        fillColor: "purple",
+        color: "none",
+        fillColor: chooseColor(geoJsonPoint.properties.mag),
         radius: geoJsonPoint.properties.mag * 4
       })
     },
@@ -47,9 +38,42 @@ function createFeaturesEarthquakes(earthquakeData) {
     },
   });
   return earthquakes
-}
+};
 
-function createMap(earthquakes,tectonics) {
+// Function that will determine the color of a circle based on the magnitude size of the earthquake
+function chooseColor(magnitude) {
+  if (magnitude <= 1) {
+    return "#FFFF00";
+  }
+  else if (magnitude <= 2) {
+    return "#FFCC00";
+  }
+  else if (magnitude <= 3) {
+    return "#FF9900";
+  }
+  else if (magnitude <= 4) {
+    return "#FF6600";
+  }
+  else if (magnitude <= 5) {
+    return "#FF3300";
+  }
+  else {
+    return "#990000"
+  }
+};
+
+
+function createFeaturesTectonics(tectonicData) {
+  console.log(tectonicData)
+  // Create a GeoJSON layer containing the features array on the tectonicData object
+  var tectonics = L.geoJSON(tectonicData, {
+    color: "orange"
+  });
+  return tectonics
+};
+
+
+function createMap(earthquakes, tectonics) {
 
   // Define streetmap and darkmap layers
   var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -78,13 +102,14 @@ function createMap(earthquakes,tectonics) {
     Tectonics: tectonics
   };
 
+
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -105.71
     ],
-    zoom: 4,
-    layers: [satellite, earthquakes]
+    zoom: 3,
+    layers: [satellite, earthquakes, tectonics]
   });
 
   // Create a layer control
@@ -93,4 +118,24 @@ function createMap(earthquakes,tectonics) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  var legend = L.control({ position: 'bottomright' });
+
+  legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5],
+      labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(Mymap);
 };
